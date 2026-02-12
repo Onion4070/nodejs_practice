f38c59e5c4d8f1195b9adbc53335b1fc60c887de
Onion4070 <naonaosub.0704@gmail.com>
2026-02-12 16:57:41 +0900

    任意のidに対するレスポンス

--- routes/user.js
+++ routes/user.js
@@ -12,4 +12,22 @@ router.get("/info", (req, res) => {
     res.send("ユーザー情報です");
 })
 
+router.get("/:id", (req, res) => {
+    // バッククォートに注意
+    res.send(`${req.params.id}のユーザ情報を取得しました`)
+
+    // ダブルクォートではすべてを文字列とみなされる
+    // res.send("${req.params.id}のユーザ情報を取得しました")
+})
+
+// post, deleteも同様に
+
+// router.post("/:id", (req, res) => {
+//     res.send(`${req.params.id}のユーザを追加しました`);
+// })
+
+// router.delete("/:id", (req, res) => {
+//     Todo
+// })
+
 module.exports = router;
